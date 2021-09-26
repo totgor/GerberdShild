ef3e7f347d1145fa112dd494a500c13f5a1aa5eb
totgor <totgor@mail.ru>
2021-09-27 01:10:38 +0300

    Область видимости переменных в блоке кода.

---
+++ src/com/gerberdshild/javalanguage/datatypesvariablesarrays/visibilityarea/VisibilityArea.java
@@ -0,0 +1,18 @@
+package com.gerberdshild.javalanguage.datatypesvariablesarrays.visibilityarea;
+
+//Область видимости переменных в блоке кода
+public class VisibilityArea {
+    public static void main(String[] args) {
+        int x; //переменная доступна всему коду
+        x = 10;
+        if (x == 10) {
+            int y = 20; //переменная доступна только этому блоку кода
+            System.out.println("x = " + x + " y = " + y);
+            x = y * 2;
+        }
+
+        //y = 100; //переменная не доступна
+        System.out.println("x = " + x);
+
+    }
+}
