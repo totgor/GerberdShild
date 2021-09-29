5c51953f415fccf4ecf23d5e6bcb286c33728bfe
totgor <totgor@mail.ru>
2021-09-29 15:15:05 +0300

    Двумерный массив.

---
+++ src/com/gerberdshild/javalanguage/datatypesvariablesarrays/array/TwoDemensionalArray.java
@@ -0,0 +1,23 @@
+package com.gerberdshild.javalanguage.datatypesvariablesarrays.array;
+
+//Двумерный массив
+public class TwoDemensionalArray {
+    public static void main(String[] args) {
+        int[][] twoD = new int[4][5];
+        int k = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                twoD[i][j] = k++;
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(twoD[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
