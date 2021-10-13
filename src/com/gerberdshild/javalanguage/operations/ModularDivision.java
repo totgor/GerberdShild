27bab88757a138bec01966f114541c5826952f94
totgor <totgor@mail.ru>
2021-10-13 19:15:41 +0300

    Деление по модулю.

---
+++ src/com/gerberdshild/javalanguage/operations/ModularDivision.java
@@ -0,0 +1,11 @@
+package com.gerberdshild.javalanguage.operations;
+
+//Деление по модулю
+public class ModularDivision {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.23;
+        System.out.println("42 mod 10 = " + x % 10);
+        System.out.println("42.23 mod 10 = " + y % 10);
+    }
+}
