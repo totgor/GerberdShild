fc9bf0183d5c45a0508fd8a1bb0e4940c47d2b09
totgor <totgor@mail.ru>
2022-05-02 21:15:22 +0300

    Ипользование запятой в for.

---
+++ src/com/gerberdshild/javalanguage/controloperators/loopoperators/CommaInFor.java
@@ -0,0 +1,29 @@
+package com.gerberdshild.javalanguage.controloperators.loopoperators;
+
+//Ипользование запятой в for.
+public class CommaInFor {
+
+    public static void NoComma() {
+        System.out.println("No Comma:");
+        int a, b;
+        b = 4;
+        for( a = 1; a < b; a++) {
+            System.out.print("a = " + a);
+            System.out.println(" b = " + b);
+            b--;
+        }
+    }
+
+    public  static void Comma() {
+        System.out.println("Comma:");
+        for(int a = 1, b = 4; a < b; a++, b--) {
+            System.out.print("a = " + a);
+            System.out.println(" b = " + b);
+        }
+    }
+
+    public static void main(String[] args) {
+        NoComma();
+        Comma();
+    }
+}
