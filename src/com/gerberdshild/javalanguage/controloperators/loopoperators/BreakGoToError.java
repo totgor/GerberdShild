4f17fed088c7aae57a638b48b36288e99b30c652
totgor <totgor@mail.ru>
2022-05-13 21:11:14 +0300

    Подобное применение оператора break как goto приведет к ошибке.

---
+++ src/com/gerberdshild/javalanguage/controloperators/loopoperators/BreakGoToError.java
@@ -0,0 +1,15 @@
+package com.gerberdshild.javalanguage.controloperators.loopoperators;
+
+//Подобное применение оператора break как goto приведет к ошибке.
+public class BreakGoToError {
+    public static void main(String[] args) {
+        one: for (int i = 0; i < 3; i++) {
+            System.out.print("Проход " + i + ": ");
+        }
+
+        for (int j = 0; j < 100; j++) {
+            //if (j == 10) break one; //Такой переход не возможен! Ошибка компиляции!
+            System.out.print(j + " ");
+        }
+    }
+}
