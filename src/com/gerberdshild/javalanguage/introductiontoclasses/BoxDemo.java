1605190b12144de4d2eb80e65c8b75d0522ba2ba
totgor <totgor@mail.ru>
2022-05-14 13:30:18 +0300

    Пример использования класса.

---
+++ src/com/gerberdshild/javalanguage/introductiontoclasses/BoxDemo.java
@@ -0,0 +1,27 @@
+package com.gerberdshild.javalanguage.introductiontoclasses;
+
+//Пример использования класса.
+
+//класс описывающий параллелипипед
+class Box {
+    double width; //ширина
+    double hight; //высота
+    double depth; //глубина
+}
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        double vol;
+
+        //присваиваем значения переменным экзмепляра класса myBox
+        myBox.width = 10;
+        myBox.hight = 20;
+        myBox.depth = 15;
+
+        //расчитать объём параллелипипеда
+        vol = myBox.width * myBox.hight * myBox.depth;
+
+        System.out.println("Объём параллелипипеда = " + vol);
+    }
+}
