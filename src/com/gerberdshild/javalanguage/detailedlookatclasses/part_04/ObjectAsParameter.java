d90fba2d89c127de4b7360c5fee088e613ad1f85
totgor <totgor@mail.ru>
2022-05-20 21:14:41 +0300

    Объект в качестве параметра метода класса.

---
+++ src/com/gerberdshild/javalanguage/detailedlookatclasses/part_04/ObjectAsParameter.java
@@ -0,0 +1,29 @@
+package com.gerberdshild.javalanguage.detailedlookatclasses.part_04;
+
+//Объект в качестве параметра метода класса.
+class Test {
+    int a;
+    int b;
+
+    Test(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    //Одинаковый с объектом в параметре метода
+    //true если одинаковый, false если отличается
+    boolean equalsTo(Test test) {
+        if (a == test.a && b == test.b) return true;
+        else return false;
+    }
+}
+
+public class ObjectAsParameter {
+    public static void main(String[] args) {
+        Test myTest1 = new Test(10, 20);
+        Test myTest2 = new Test(10, 20);
+        Test myTest3 = new Test(-1, -1);
+        System.out.println("myTest1 == myTest2: " + myTest1.equalsTo(myTest2));
+        System.out.println("myTest1 == myTest3: " + myTest1.equalsTo(myTest3));
+    }
+}
