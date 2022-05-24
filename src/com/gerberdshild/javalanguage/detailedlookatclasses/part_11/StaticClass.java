e36ae3e35555b3d856945bf864573703aa7f8961
totgor <totgor@mail.ru>
2022-05-24 16:22:33 +0300

    Пример использования статических static переменных, методов и блока кода.

---
+++ src/com/gerberdshild/javalanguage/detailedlookatclasses/part_11/StaticClass.java
@@ -0,0 +1,24 @@
+package com.gerberdshild.javalanguage.detailedlookatclasses.part_11;
+
+//Пример использования статических static переменных, методов и блока кода.
+public class StaticClass {
+    static int a = 3; //1. как только загружается статический класс StaticClass, выполняется все статические операторы
+    static int b;
+
+    static void staticMethod(int x) {
+        System.out.println("x = " + x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    //2. затем выполняется статический блок кода
+    static {
+        System.out.println("Инициализация статического блока.");
+        b = a * 4;
+    }
+
+    //3. выполняется метод main(), который вызывает метод staticMethod()
+    public static void main(String[] args) {
+        staticMethod(38);
+    }
+}
