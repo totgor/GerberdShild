17d0d7cb7b3352baf3e8940a643947f979a8b944
totgor <totgor@mail.ru>
2022-06-18 15:13:57 +0300

    Пример использования константных переменных в интерфесах.

---
+++ src/com/gerberdshild/javalanguage/packagesandinterfaces/part_09/Question.java
@@ -0,0 +1,18 @@
+package com.gerberdshild.javalanguage.packagesandinterfaces.part_09;
+
+import java.util.Random;
+
+class Question implements SharedConstants {
+    private Random rand = new Random(); //создаем объект для получения псевдослучайных чисел
+
+    //получить псевдослучайное число от 0 до 6
+    int ask() {
+        int value = (int) (100 * rand.nextDouble()); //полчить вещественное число от 0.0 до 1.0, довести до 2х значного, преобразовать к целочисленному int-у, отбросив дробь
+        if (value < 25) return NO; //25%
+        else if (value < 50) return YES; //25%
+        else if (value < 65) return MAYBE; //15%
+        else if (value < 75) return LATER; //10%
+        else if (value < 98) return SOON;  //13%
+        else return NEVER; //2%
+    }
+}
---
+++ src/com/gerberdshild/javalanguage/packagesandinterfaces/part_09/SharedConstants.java
@@ -0,0 +1,11 @@
+package com.gerberdshild.javalanguage.packagesandinterfaces.part_09;
+
+//Интерфейс констант для вариантов ответов.
+interface SharedConstants {
+    int NO = 0;
+    int YES = 1;
+    int MAYBE = 3;
+    int LATER = 4;
+    int SOON = 5;
+    int NEVER = 6;
+}
