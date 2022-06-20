3d351e765a4450fd79b00483895cc1cb2c4ca2c4
totgor <totgor@mail.ru>
2022-06-20 22:51:53 +0300

    Пример использования статического метода в интерфейсе.

---
+++ src/com/gerberdshild/javalanguage/packagesandinterfaces/part_12/StaticMethodDemo.java
@@ -0,0 +1,8 @@
+package com.gerberdshild.javalanguage.packagesandinterfaces.part_12;
+
+//Пример использования статического метода в интерфейсе.
+public class StaticMethodDemo {
+    public static void main(String[] args) {
+        System.out.println("Вызываем статический метод в интерфейсе MyInterface.getDefaultNumber() = " + MyInterface.getDefaultNumber());
+    }
+}
