e06330264bc6dbf08ec3bde1b43bec796084269d
totgor <totgor@mail.ru>
2022-06-27 14:59:35 +0300

    Пример многократного перехвата исключений.

---
+++ src/com/gerberdshild/javalanguage/exceptionhandling/part_13/MultiCatch.java
@@ -0,0 +1,33 @@
+package com.gerberdshild.javalanguage.exceptionhandling.part_13;
+
+//Пример многократного перехвата исключений.
+public class MultiCatch {
+    static void proc(int variant) {
+        int a = 10, b =0, result;
+        int[] array = {1, 2, 3};
+
+        try {
+            switch(variant){
+                case 1:
+                    result = a / b;
+                    break;
+                case 2:
+                    array[10] = 777;
+                    break;
+                default:
+                    System.out.println("Такого варианта не существует.");
+            }
+            //В этом операторе catch перехватываются оба типа исключений.
+            //Каждый параметр многократного перехвата неявно считается конечным final.Поэтому новое значение ему присвоить нельзя
+        } catch (ArithmeticException |
+                 IndexOutOfBoundsException e) {
+            System.out.println("Исключение перехвачено: " + e);
+        }
+        System.out.println("После многократного перехвата.");
+    }
+
+    public static void main(String[] args) {
+        proc(1);
+        proc(2);
+    }
+}
