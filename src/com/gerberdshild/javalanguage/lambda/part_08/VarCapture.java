318c6f8470222724672d61928c87133b6a7f1301
totgor <totgor@mail.ru>
2022-09-04 22:40:32 +0300

    Пример захвата локальной переменной из объемлющий области действия. Дополнения.

--- src/com/gerberdshild/javalanguage/lambda/part_08/VarCapture.java
+++ src/com/gerberdshild/javalanguage/lambda/part_08/VarCapture.java
@@ -6,19 +6,36 @@ interface MyFunction {
 }
 
 public class VarCapture {
-    public static void main(String[] args) {
-        int num = 10; // Эта локальная переменная будет захвачена.
+    
+    static int static_value;
+    
+    static void method() {
+        System.out.println("Это method().");
+    }
 
+    public static void main(String[] args) {
+        int local_number = 10; // Эта локальная переменная будет захвачена.
+        
         MyFunction myFunction = (n) -> {
-            // Т.к. num не изменяется, то такое применение num допустимо.
-            int value = num + 10;
+            // Вызов метода из объемлющего класса допустим.
+            method();
+
+            //Изменение значения статической переменной из объемлющего класса, захват переменной не происходит.
+            static_value = 10;
+            static_value ++;            
+            System.out.println("Значение static_value = " + static_value);            
+
+            // Т.к. local_number не изменяется, то такое применение local_number допустимо.
+            int value = local_number + static_value;
             return value;
+            
             // Потпытка изменить захваченную переменную не допустима, теоретически она final.
-            //num++;
+            // local_number++;
         };
 
+        System.out.println("Вызов лямбда-выражения: " + myFunction.function(100));
         // Действительно конечная переменная, т.к. она была использована в лямбда выражении.
         // Если её не использовать в лямбда-выражении, то изменять её можно.
-        //num = 9;
+        // local_number = 9;
     }
 }
